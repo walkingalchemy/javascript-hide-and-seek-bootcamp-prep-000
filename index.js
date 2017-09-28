50e7f375c4efa954f88828180d86a925d9843564
James Royer <walkingalchemy@gmail.com>
2017-09-28 17:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var ranked = document.querySelectorAll(".ranked-list li")
   for (let i = 0; i < ranked.length; i++) {
-    ranked[i].innerHTML += n
+    parseInt(ranked[i].innerHTML) += n
   }
 }
